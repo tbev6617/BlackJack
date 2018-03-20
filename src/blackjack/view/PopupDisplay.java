0eb4367a44272c03f8ec7231d434a9542eb6f89c
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2018-03-20 09:12:34 -0600

    Added a cartoony card image

--- src/blackjack/view/PopupDisplay.java
+++ src/blackjack/view/PopupDisplay.java
@@ -7,20 +7,22 @@ public class PopupDisplay
 {
 	
 	private String windowTitle;
+	private ImageIcon icon;
 	
 	public PopupDisplay()
 	{
+		icon = new ImageIcon(getClass().getResource("images/cartoonCards.jpg"));
 		windowTitle = "Popup";
 	}
 	
 	public void displayText(String message)
 	{
-		JOptionPane.showMessageDialog(null, message, windowTitle, JOptionPane.INFORMATION_MESSAGE, null);
+		JOptionPane.showMessageDialog(null, message, windowTitle, JOptionPane.INFORMATION_MESSAGE, icon);
 	}
 	public String getResponse(String question)
 	{
 		String answer = "";
-		answer += JOptionPane.showInputDialog(null, question, windowTitle, JOptionPane.PLAIN_MESSAGE, null, null, "");
+		answer += JOptionPane.showInputDialog(null, question, windowTitle, JOptionPane.PLAIN_MESSAGE, icon, null, "");
 		return answer;
 	}
 }
